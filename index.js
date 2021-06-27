9c2ccb7d5820c4a7142f6e1381aeb707f203ea2e
Guilherme Dias Redu <guilisk8@gmail.com>
2021-06-27 19:31:38 -0300

    V1

---
+++ index.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const httpProxy = require('http-proxy');
+const config = require('./domains.json');
+
+let proxy = httpProxy.createProxyServer();
+console.log(new Date, '[SERVER] Started');
+
+let server = http.createServer((req, res) => {
+  let host = req.headers.host;
+
+  if (config[host]) {
+    console.log(new Date, `[REQUEST] Proxied from ${host} to ${config[host]}`);
+    return proxy.web(req, res, {
+      target: `http://localhost:${config[host]}`,
+      secure: false
+    });
+  }
+
+  console.error(new Date, '[REQUEST] Host not found', host);
+  res.statusCode = 404;
+  res.end();
+}).listen(80);
+
+proxy.on('error', (err, req, res) => {
+  console.error(new Date, '[ERROR] ', err);
+  res.statusCode = 404;
+  res.end();
+});
